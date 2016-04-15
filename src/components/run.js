4a48046893837fb1bf6d2bb4cd1745010bb48c52
Alex Holachek <aholachek@gmail.com>
2016-04-15 11:38:07 -0400

    initial

---
+++ src/components/run.js
@@ -0,0 +1,31 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './Main';
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { Router, Route, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
+
+import appReducer from './../reducers/reducer';
+
+// Add the reducer to your store on the `routing` key
+const store = createStore(
+  appReducer
+)
+
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(browserHistory, store)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+       <IndexRoute></IndexRoute>
+        <Route path="search" component={Results}/>
+        <Route path="abstract" component={Abstract}/>
+      </Route>
+    </Router>
+  </Provider>,
+  document.getElementById('app')
+)
---
+++ src/reducers/reducer.js
@@ -0,0 +1,18 @@
+
+import { routerReducer } from 'react-router-redux'
+import { combineReducers } from 'redux'
+import initialState from './../store/initial-state'
+
+function currentQuery( state = initialState, action){
+  return state
+}
+
+function currentBibcode( state = initialState, action){
+  return state
+}
+
+export default combineReducers({
+  currentQuery,
+  currentBibcode,
+  routerReducer
+})
